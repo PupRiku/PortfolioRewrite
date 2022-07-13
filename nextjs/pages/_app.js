88b8bafb2d5fc0670c3f6804031eebf86bf50523
Chris Diorio <chris.diorio12@gmail.com>
2022-07-13 16:56:38 -0500

    Setup Header

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,27 +1,58 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
-import theme from '../src/theme';
-import createEmotionCache from '../src/createEmotionCache';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import { CacheProvider } from "@emotion/react";
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
+import Fonts from "../src/ui/Fonts";
+import Header from "../src/ui/Header";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
+  const [experienceIndex, setExperienceIndex] = useState(0);
+  const [lifeIndex, setLifeIndex] = useState(0);
+  const [value, setValue] = useState(0);
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  React.useEffect(() => {
+    Fonts();
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>Chris Diorio</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
+        <Header
+          value={value}
+          setValue={setValue}
+          lifeIndex={lifeIndex}
+          setLifeIndex={setLifeIndex}
+          experienceIndex={experienceIndex}
+          setExperienceIndex={setExperienceIndex}
+        />
+        <Component
+          {...pageProps}
+          value={value}
+          setValue={setValue}
+          lifeIndex={lifeIndex}
+          setLifeIndex={setLifeIndex}
+          experienceIndex={experienceIndex}
+          setExperienceIndex={setExperienceIndex}
+        />
       </ThemeProvider>
     </CacheProvider>
   );
--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,24 +1,145 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import React from "react";
+import Head from "next/head";
+
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import CardHeader from "@mui/material/CardHeader";
+
+import Link from "../src/Link";
+import theme from "../src/theme";
+import Header from "../src/ui/Header";
+
+const mainContainer = {
+  [theme.breakpoints.down("sm")]: {
+    marginTop: "3em",
+  },
+};
+const heroTextContainer = {
+  minWidth: "21.5em",
+  marginLeft: "4em",
+  marginRight: "1em",
+  [theme.breakpoints.down("sm")]: {
+    marginLeft: 0,
+  },
+};
+
+const heroText = {
+  fontFamily: "Raleway",
+  fontWeight: 700,
+  fontSize: "2.25em",
+  color: theme.palette.common.orange,
+  [theme.breakpoints.down("lg")]: {
+    fontSize: "1.75em",
+  },
+};
+
+const heroImage = {
+  marginLeft: 0,
+  borderRadius: 30,
+  boxShadow: theme.shadows[10],
+};
+
+const introText = {
+  fontSize: "1.25em",
+  fontWeight: 300,
+};
+
+const philosophyBackground = {
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  height: "100%",
+  width: "100%",
+};
+
+const philosophyCard = {
+  position: "absolute",
+  boxShadow: theme.shadows[10],
+  borderRadius: 15,
+  maxWidth: 1000,
+  backgroundColor: theme.palette.common.white,
+  [theme.breakpoints.down("lg")]: {
+    maxWidth: 800,
+  },
+  [theme.breakpoints.down("md")]: {
+    maxWidth: 500,
+  },
+};
+
+const philosophyText = {
+  color: theme.palette.common.white,
+  fontSize: "1.25em",
+  fontWeight: 300,
+};
+
+const philosophyButton = {
+  marginTop: "2em",
+  backgroundColor: theme.palette.common.orange,
+  color: theme.palette.common.white,
+  textTransform: "none",
+  borderRadius: 50,
+  fontFamily: "Roboto",
+  fontWeight: "bold",
+  height: 35,
+  padding: 10,
+  "&:hover": {
+    backgroundColor: theme.palette.primary.light,
+  },
+};
+
+const portfolioArrows = {
+  backgroundColor: theme.palette.common.orange,
+  color: theme.palette.common.white,
+  borderRadius: 10,
+  padding: "14px 14px",
+  marginLeft: "2em",
+  marginRight: "2em",
+  "&:hover": {
+    backgroundColor: theme.palette.primary.light,
+  },
+};
+
+const portfolioButton = {
+  backgroundColor: theme.palette.common.orange,
+  color: theme.palette.common.white,
+  textTransform: "none",
+  borderRadius: 50,
+  fontFamily: "Roboto",
+  fontWeight: "bold",
+  fontSize: "1.1rem",
+  paddingLeft: 25,
+  paddingRight: 25,
+  "&:hover": {
+    backgroundColor: theme.palette.primary.light,
+  },
+};
+
+const greyImage = {
+  filter: "gray",
+  WebkitFilter: "grayscale(1)",
+  WebkitTransition: "all .5s ease-in-out",
+  zIndex: "auto",
+  "&:hover": {
+    filter: "none",
+    zIndex: 10,
+    WebkitFilter: "grayscale(0)",
+    transform: "scale(1.05)",
+    boxShadow: theme.shadows[10],
+  },
+};
+
+const titleContainer = {
+  marginBottom: "10em",
+};
+
+const highlight = {
+  color: theme.palette.common.purple,
+  fontWeight: 700,
+};
 
 export default function Index() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+  return <div>Hello!</div>;
 }
